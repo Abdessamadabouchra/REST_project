97991030aeb6770574a7fb73af0de4f7008053e7
Et-taky <160367722+Et-taky@users.noreply.github.com>
2025-10-28 00:15:58 +0100

    Delete src/main/java/com/project1/project1/Repository directory

--- src/main/java/com/project1/project1/Repository/PostDao.java
+++
@@ -1,9 +0,0 @@
-package com.project1.project1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project1.project1.model.Post;
-
-public interface PostDao extends JpaRepository<Post, Integer> {
-    
-}
