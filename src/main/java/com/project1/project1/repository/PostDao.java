48ca950e950ab472cbf25a7de0b46c099e9b53dd
abdessamad <abdessamadabouchraa20@gmail.com>
2025-10-28 00:20:15 +0100

    new dtos added

---
+++ src/main/java/com/project1/project1/repository/PostDao.java
@@ -0,0 +1,7 @@
+import com.project1.project1.model.Post;        
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PostDao extends JpaRepository<Post, Integer> {
+
+}
---
+++ src/main/java/com/project1/project1/repository/UserDao.java
@@ -0,0 +1,6 @@
+import com.project1.project1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao  extends JpaRepository<User, Integer> {
+    
+}
