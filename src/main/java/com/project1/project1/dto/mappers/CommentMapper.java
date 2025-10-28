c7c8cbf72bd4386c6871f221857959aa1e396665
unknown <ettakyabdeljalil2022@gmail.com>
2025-10-28 22:35:52 +0100

    mapstruct adding

--- src/main/java/com/project1/project1/dto/mappers/CommentMapper.java
+++ src/main/java/com/project1/project1/dto/mappers/CommentMapper.java
@@ -1,5 +1,14 @@
 package com.project1.project1.dto.mappers;
 
+import com.project1.project1.dto.CommentDto;
+import com.project1.project1.model.Comment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+
 public interface CommentMapper {
 
+    CommentDto commentToCommentDto(Comment comment);
+
+    Comment commentDtoToComment(CommentDto commentDto);
 }
