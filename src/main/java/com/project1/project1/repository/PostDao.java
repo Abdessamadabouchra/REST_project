59ed437e146133d200ae06f802c1e00f190862ae
abdessamad <abdessamadabouchraa20@gmail.com>
2025-10-28 22:04:08 +0100

    changes added

--- src/main/java/com/project1/project1/repository/PostDao.java
+++ src/main/java/com/project1/project1/repository/PostDao.java
@@ -1,3 +1,5 @@
+package com.project1.project1.repository;
+
 import com.project1.project1.model.Post;        
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/com/project1/project1/repository/UserDao.java
+++ src/main/java/com/project1/project1/repository/UserDao.java
@@ -1,3 +1,5 @@
+package com.project1.project1.repository;
+
 import com.project1.project1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
