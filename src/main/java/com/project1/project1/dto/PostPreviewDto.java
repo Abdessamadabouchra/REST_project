69219e0a3afc06078a8cbd70dd2d522eb1048c42
abdessamad <abdessamadabouchraa20@gmail.com>
2025-10-29 18:33:22 +0100

    Post_DTO and post mapper and userpreview dto created

---
+++ src/main/java/com/project1/project1/dto/PostPreviewDto.java
@@ -0,0 +1,69 @@
+package com.project1.project1.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.validation.constraints.Size;
+
+public class PostPreviewDto {
+
+    private Integer id;
+    private String text;
+    private String image;
+    private int likes;
+    private List<String> tags;
+      @Size(min = 6, max = 600)
+    private String link;
+    private LocalDate publishDate;
+    private UserPreviewDto owner;
+    public Integer getId() {
+        return id;
+    }   
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }                   
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public int getLikes() {
+        return likes;
+    }
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+    public List<String> getTags() {
+        return tags;
+    }
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+    public UserPreviewDto getOwner() {
+        return owner;
+    }
+    public void setOwner(UserPreviewDto owner) {
+        this.owner = owner;
+    }
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+}
--- src/main/java/com/project1/project1/dto/mappers/PostMapper.java
+++ src/main/java/com/project1/project1/dto/mappers/PostMapper.java
@@ -1,21 +1,39 @@
 package com.project1.project1.dto.mappers;
 
 import com.project1.project1.dto.PostDto;
+import com.project1.project1.dto.PostPreviewDto;
+import com.project1.project1.dto.UserPreviewDto;
 import com.project1.project1.model.Post;
+import com.project1.project1.model.User;
+
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
-    @Mapping(source = "owner.id", target = "ownerId")
-    @Mapping(source = "owner.title", target = "ownerTitle")
-    @Mapping(source = "owner.firstName", target = "ownerFirstName")
-    @Mapping(source = "owner.lastName", target = "ownerLastName")
-    @Mapping(source = "owner.picture", target = "ownerPicture")
-    PostDto toDto(Post post);
+    // Création : PostDto -> Post entity
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "publishDate", ignore = true)
+    @Mapping(source = "ownerId", target = "owner.id")
+    Post toEntity(PostPreviewDto dto);
 
-    @Mapping(source = "ownerId", target = "owner", ignore = true)
-    Post toEntity(PostDto dto);
+    // Preview : Post entity -> PostDto
+    @Mapping(source = "owner", target = "owner") // MapStruct va utiliser toUserPreviewDto
+    PostPreviewDto toDto(Post post);
+
+    // Full return : Post entity -> PostResponseDto
+    PostDto toResponseDto(Post post);
+
+    // Mapping liste
+    List<PostPreviewDto> toDtoList(List<Post> posts);
+    List<PostDto> toResponseDtoList(List<Post> posts);
+
+    // Méthode pour mapper User -> UserPreviewDto
+    UserPreviewDto toUserPreviewDto(User user);
+
+    PostPreviewDto updatePostDto(PostPreviewDto postPreviewDto,@MappingTarget Post post);
 }
