6ed95d5fceff6b82afb175f7d04f6f2037b9f1dd
abdessamad <abdessamadabouchraa20@gmail.com>
2025-11-21 02:11:13 +0100

    tag cache impl

--- src/main/java/com/project1/project1/controller/TagController.java
+++ src/main/java/com/project1/project1/controller/TagController.java
@@ -1,13 +1,19 @@
 package com.project1.project1.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project1.project1.services.TagService;
+import com.project1.project1.util.GenerateEtag;
 
 @RestController
 @RequestMapping("/v1/tags")
@@ -17,7 +23,21 @@ public class TagController {
     private TagService tagService;
 
     @GetMapping
-    public List<String> getTags() {
-        return tagService.getAllTags();
+    public ResponseEntity<List<String>> getTags(
+            @RequestHeader(value = "If-None-Match", required = false) String ifNoneMatch) {
+        List<String> tags = tagService.getAllTags();
+        String eTag = GenerateEtag.generate(tags);
+
+        if (ifNoneMatch != null && ifNoneMatch.equals(eTag)) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED)
+                    .eTag(eTag)
+                    .build();
+        }
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS).cachePublic())
+                .eTag(eTag)
+                .body(tags);
     }
+
 }
